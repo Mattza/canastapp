81a621fd876021a88340929d98b1dde8d915ac94
Mattias Knutsson <matknu@kth.se>
2016-11-22 13:32:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,29 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
     response.render('index');
 });
-
+app.get('/bids/',function(request,response){
+    getBids((bids)=>{
+          response.status(200);
+          response.send(bids);
+    })
+});
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
 
-
+var http = require('https');
+var getBids = (cb) => {
+    http.get({
+        host: 'objekt.fastighetsbyran.se',
+        path: '/Objekt/Budgivning/GetSenasteBud?ObjektId=1636306',
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13'
+        }
+    }, (response) => {
+        response.on('data', (data) => {
+            cb(cheerio.load(data)('.bidsTable .bid').slice(1).map(function (i, link) {
+                return cheerio(link).text().replace(/ |kr|\n|\r/g, '').toString();
+            }).toArray());
+        });
+    });
+}
